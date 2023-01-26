95d444cd2bea5380b1d8efd4ac06b3556ae7acb5
BigThinkerYes <designaspnet4@hotmail.com>
2023-01-26 16:03:47 -0600

    4-out-of-6-passed-last-2-in-progress

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const { User } = require('./db');
+const bcrypt = require('bcrypt');
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -16,9 +18,60 @@ app.get('/', async (req, res, next) => {
 
 // POST /register
 // TODO - takes req.body of {username, password} and creates a new user with the hashed password
+app.post('/register', async (req, res, next) => {
+  try{
+    const {username, password} = req.body;
+    const user = await User.create({username : 'bobbysmiles', password :'youllneverguess'})
+//     
+if(!user || !password){
+  return res.status(401).send(
+    'incorrect username or password'
+  );
+} else {
+    return res.status(200).send(
+      'successfully created user bobbysmiles'
+    )
+}
+}catch(error){
+console.log(error.message);
+next(error);
+}
+})
 
+
+// ;;;;;;;;;;;;;;;;;;
+// const hashPassword = async (password) =>{
+//   const hash = await bcrypt.hash(password, 8);
+//   console.log(hash);   
+//   return hash;
+// }
 // POST /login
 // TODO - takes req.body of {username, password}, finds user by username, and compares the password with the hashed version from the DB
 
+app.post('/login', async (req, res, next) => {
+  try{
+    const loginA = {username: 'bobbysmiles', password: 'notright'};
+    const users = await User.findOne({where: {username: loginA.username}});
+    if(!users[0]){
+      return res.status(401).send(
+       'incorrect username or password'
+      );
+    } 
+    const user = users[0];
+    const isMatch = await bcrypt.compare(loginA.password, user.password);
+    if(!isMatch){
+    return res.status(401).send(
+      'incorrect username or password'
+    )
+  } else{
+    return res.status(200).send(
+    'successfully logged in user bobbysmiles'
+    )
+  }
+  }catch(error){
+    console.log(error.message);
+    next(error);
+  }
+})
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
